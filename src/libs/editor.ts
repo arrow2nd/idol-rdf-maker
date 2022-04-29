87d12b4f287ad94d794aefd31f73e7dd745dfaa9
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-04-29 12:23:59 +0900

    feat(libs): テキスト挿入時にインデントを考慮する

--- src/libs/editor.ts
+++ src/libs/editor.ts
@@ -7,5 +7,12 @@ import type { TextEditor } from 'vscode'
  */
 export async function insertEditor(editor: TextEditor, text: string) {
   const cursorPos = editor.selection.active
+  const lineText = editor.document.lineAt(cursorPos.line).text
+
+  // 挿入する行を見て、インデントを合わせる
+  if (/^\s+$/.test(lineText)) {
+    text = text.replace(/\n/g, `\n${lineText}`)
+  }
+
   await editor.edit((editBuilder) => editBuilder.insert(cursorPos, text))
 }
