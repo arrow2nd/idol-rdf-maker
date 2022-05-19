8ba027faa188ced4e88913074476b251263d564f
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-19 21:43:54 +0900

    chore(data): CDデータ用の語彙を追加

---
+++ src/data/releaseFormat.ts
@@ -0,0 +1,12 @@
+import { QuickPickItem } from 'vscode'
+
+export const releaseFormatQuickPickItems: QuickPickItem[] = [
+  {
+    label: 'CDFormat',
+    detail: 'CD'
+  },
+  {
+    label: 'DigitalFormat',
+    detail: 'デジタル配信'
+  }
+]
---
+++ src/data/releaseType.ts
@@ -0,0 +1,16 @@
+import { QuickPickItem } from 'vscode'
+
+export const releaseTypeQuickPickItems: QuickPickItem[] = [
+  {
+    label: 'AlbumRelease',
+    detail: 'アルバム'
+  },
+  {
+    label: 'EPRelease',
+    detail: 'EP盤'
+  },
+  {
+    label: 'SingleRelease',
+    detail: 'シングル'
+  }
+]
